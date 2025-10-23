bd210ca7bf6cc337287b90e4a8d80c9c74b0d79a
Yannlhotel <yann.lhotelier@orange.fr>
2025-10-23 14:10:31 +0200

    Push jusqu'à 1.4

--- Yann/lfi-01/01_filter.py
+++ Yann/lfi-01/01_filter.py
@@ -32,7 +32,7 @@ def convolution_2d(img, kernel) -> np.ndarray:
     :param kernel: convolution matrix - 3x3, or 5x5 matrix
     :return: result of the convolution
     """
-    # TODO write convolution of arbritrary sized convolution here
+    # TO DO write convolution of arbritrary sized convolution here
     # Attention: convolution should work with kernels of any size.
     # Tip: use Numpy as good as possible, otherwise the algorithm will take too long.
     # Tip: Use Padding, the output image should be the same size of the input without
@@ -41,31 +41,40 @@ def convolution_2d(img, kernel) -> np.ndarray:
 
 
     offset = int(kernel.shape[0]/2)
+    print(offset)
     newimg = np.zeros(img.shape)
 
-    # YOUR CODE HERE
-
+    padded_img = np.pad(img, pad_width=offset, mode='edge')
+    newimg = np.zeros(img.shape)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            region_padded_img = padded_img[i:i+2*offset+1, j:j+2*offset+1]
+            newimg[i,j] = np.sum(region_padded_img * kernel)
+        
     return newimg
 
 
 if __name__ == "__main__":
-
-    # 1. load image in grayscale
     
+    # 1. load image in grayscale
+    img = cv2.imread("Yann/lfi-01/graffiti.png", cv2.IMREAD_GRAYSCALE)
 
     # image kernels
     sobelmask_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     sobelmask_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
-    gk = make_gaussian(11)
-
+    gk = make_gaussian(51)
+    # Apply Gaussian filter
+    img = convolution_2d(img, gk)
     # 2 use image kernels
-
+    sobel_x = convolution_2d(img, sobelmask_x)
+    sobel_y = convolution_2d(img, sobelmask_y)
     # 3. compute magnitude of gradients
-
+    mog = np.sqrt(sobel_x**2 + sobel_y**2)
     # Show resulting images
     # Note: sobel_x etc. must be uint8 images to get displayed correctly astype(np.uint8)
-    cv2.imshow("sobel_x", sobel_x) #type: ignore
-    cv2.imshow("sobel_y", sobel_y) #type: ignore
-    cv2.imshow("mog", mog)         #type: ignore
+    cv2.imshow("Original image", img)
+    cv2.imshow("sobel_x", sobel_x.astype(np.uint8)) 
+    cv2.imshow("sobel_y", sobel_y.astype(np.uint8)) 
+    cv2.imshow("mog", mog.astype(np.uint8))         
     cv2.waitKey(0)
     cv2.destroyAllWindows()
